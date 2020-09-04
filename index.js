a7a3a81965de7914121b769b90fa5ff8f23dc79e
drormaman <drormaman@gmail.com>
2020-09-04 16:32:13 +0300

    added all necessary routs

--- index.js
+++ index.js
@@ -6,6 +6,31 @@ const Tutorial = require("./models/tutorial");
 
 app.use(express.json());
 
+// GET - get all tutorials
+/*
+api/tutorials?title=kw
+find all Tutorials which title contains 'kw'
+*/
+app.get("/api/tutorials", (req, res) => {});
+
+// GET - get tutorial by id
+app.get("/api/tutorials/:id", (req, res) => {});
+
+// POST - add new tutorial
+app.post("/api/tutorials", (req, res) => {});
+
+// PUT - update tutorial by id
+app.put("/api/tutorials/:id", (req, res) => {});
+
+// DELETE - remove tutorial by id
+app.delete("/api/tutorials/:id", (req, res) => {});
+
+// DELETE - remove all tutorials
+app.delete("/api/tutorials", (req, res) => {});
+
+// GET - find all published Tutorials
+app.get("api/tutorials/published", (req, res) => {});
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
